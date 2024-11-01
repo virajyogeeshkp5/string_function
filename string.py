e1a330be7440bdfb514b9176d0af69c020f3a185
YOGEESH K P <yogeesh793@gmail.com>
2024-11-01 23:57:14 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,11 @@
+str1="python"
+str2='computer language'
+str3="'we are learning python string'"
+print(str1,"\n",str2,"\n",str3)
+print(str1[5])
+print(str2[-1])
+
+# string function
+print(len(str1))
+a=str1.index('n')
+print(a)
